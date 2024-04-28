5a800874260d3eea4010b39b2df229d95845ea5a
rubanik00 <orubanik.it@gmail.com>
2024-04-28 12:18:33 +0200

    developed the first part with internal and support functions + tests

---
+++ test/TakeProfitsHook.t.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// Foundry libraries
+import {Test} from "forge-std/Test.sol";
+
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+
+// Our contracts
+import {TakeProfitsHook} from "../src/TakeProfitsHook.sol";
+import {HookMiner} from "./utils/HookMiner.sol";
+
+contract TakeProfitsHookTest is Test, Deployers {
+    // Use the libraries
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+
+    // The two currencies (tokens) from the pool
+    Currency token0;
+    Currency token1;
+
+    TakeProfitsHook hook;
+
+    function setUp() public {
+        // Deploy v4 core contracts
+        deployFreshManagerAndRouters();
+
+        // Deploy two test tokens
+        (token0, token1) = deployMintAndApprove2Currencies();
+
+        // Deploy our hook
+        uint160 flags = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.AFTER_SWAP_FLAG);
+        (, bytes32 salt) =
+            HookMiner.find(address(this), flags, 0, type(TakeProfitsHook).creationCode, abi.encode(manager, ""));
+        hook = new TakeProfitsHook{salt: salt}(manager, "");
+
+        // Approve our hook address to spend these tokens as well
+        MockERC20(Currency.unwrap(token0)).approve(address(hook), type(uint256).max);
+        MockERC20(Currency.unwrap(token1)).approve(address(hook), type(uint256).max);
+
+        // Initialize a pool with these two tokens
+        (key,) = initPool(token0, token1, hook, 3000, SQRT_RATIO_1_1, ZERO_BYTES);
+
+        // Add initial liquidity to the pool
+
+        // Some liquidity from -60 to +60 tick range
+        modifyLiquidityRouter.modifyLiquidity(
+            key,
+            IPoolManager.ModifyLiquidityParams({tickLower: -60, tickUpper: 60, liquidityDelta: 10 ether}),
+            ZERO_BYTES
+        );
+        // Some liquidity from -120 to +120 tick range
+        modifyLiquidityRouter.modifyLiquidity(
+            key,
+            IPoolManager.ModifyLiquidityParams({tickLower: -60, tickUpper: 60, liquidityDelta: 10 ether}),
+            ZERO_BYTES
+        );
+        // some liquidity for full range
+        modifyLiquidityRouter.modifyLiquidity(
+            key,
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: TickMath.minUsableTick(60),
+                tickUpper: TickMath.maxUsableTick(60),
+                liquidityDelta: 10 ether
+            }),
+            ZERO_BYTES
+        );
+    }
+
+    function test_placeOrder() public {
+        // Place a zeroForOne take-profit order
+        // for 10e18 token0 tokens
+        // at tick 100
+        int24 tick = 100;
+        uint256 amount = 10e18;
+        bool zeroForOne = true;
+
+        // Note the original balance of token0 we have
+        uint256 originalBalance = token0.balanceOfSelf();
+
+        // Place the order
+        int24 tickLower = hook.placeOrder(key, tick, zeroForOne, amount);
+
+        // Note the new balance of token0 we have
+        uint256 newBalance = token0.balanceOfSelf();
+
+        // Since we deployed the pool contract with tick spacing = 60
+        // i.e. the tick can only be a multiple of 60
+        // the tickLower should be 60 since we placed an order at tick 100
+        assertEq(tickLower, 60);
+
+        // Ensure that our balance of token0 was reduced by `amount` tokens
+        assertEq(originalBalance - newBalance, amount);
+
+        // Check the balance of ERC-1155 tokens we received
+        uint256 positionId = hook.getPositionId(key, tickLower, zeroForOne);
+        uint256 tokenBalance = hook.balanceOf(address(this), positionId);
+
+        // Ensure that we were, in fact, given ERC-1155 tokens for the order
+        // equal to the `amount` of token0 tokens we placed the order for
+        assertTrue(positionId != 0);
+        assertEq(tokenBalance, amount);
+    }
+
+    function test_cancelOrder() public {
+        // Place an order as earlier
+        int24 tick = 100;
+        uint256 amount = 10e18;
+        bool zeroForOne = true;
+
+        uint256 originalBalance = token0.balanceOfSelf();
+        int24 tickLower = hook.placeOrder(key, tick, zeroForOne, amount);
+        uint256 newBalance = token0.balanceOfSelf();
+
+        assertEq(tickLower, 60);
+        assertEq(originalBalance - newBalance, amount);
+
+        // Check the balance of ERC-1155 tokens we received
+        uint256 positionId = hook.getPositionId(key, tickLower, zeroForOne);
+        uint256 tokenBalance = hook.balanceOf(address(this), positionId);
+        assertEq(tokenBalance, amount);
+
+        // Cancel the order
+        hook.cancelOrder(key, tickLower, zeroForOne);
+
+        // Check that we received our token0 tokens back, and no longer own any ERC-1155 tokens
+        uint256 finalBalance = token0.balanceOfSelf();
+        assertEq(finalBalance, originalBalance);
+
+        tokenBalance = hook.balanceOf(address(this), positionId);
+        assertEq(tokenBalance, 0);
+    }
+
+    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external pure returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(address, address, uint256[] calldata, uint256[] calldata, bytes calldata)
+        external
+        pure
+        returns (bytes4)
+    {
+        return this.onERC1155BatchReceived.selector;
+    }
+}
